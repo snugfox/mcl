1d46b61709c5a870702f6b4f9fdf425c16e1c3bf
snugfox <snugfox@users.noreply.github.com>
2020-03-24 18:46:21 -0400

    Mount Docker sock in build container

--- build/package/docker/release.sh
+++ build/package/docker/release.sh
@@ -3,5 +3,6 @@
 BUILD_IMAGE="$1"
 
 docker run --rm -e DOCKER_USERNAME -e DOCKER_PASSWORD -e GITHUB_TOKEN \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$(pwd):/go/src/github.com/snugfox/mcl" ${BUILD_IMAGE} \
     goreleaser release --rm-dist
