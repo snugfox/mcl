3e060883b10a5594c36ce8b5fb1e12aa17588539
snugfox <snugfox@users.noreply.github.com>
2020-03-24 20:05:01 -0400

    Push Docker image to GitHub packages

--- build/release.sh
+++ build/release.sh
@@ -2,7 +2,10 @@
 
 BUILD_IMAGE="$1"
 
-docker run --rm -e DOCKER_USERNAME -e DOCKER_PASSWORD -e GITHUB_TOKEN \
+echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
+echo "${GITHUB_TOKEN}" | docker login docker.pkg.github.com -u snugfox --password-stdin
+
+docker run --rm -e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD -e GITHUB_TOKEN \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$(pwd):/go/src/github.com/snugfox/mcl" ${BUILD_IMAGE} \
     goreleaser release --rm-dist
