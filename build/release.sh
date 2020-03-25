bb7febf7cf28aa8d4754d58424a4f8fba5bea5e4
snugfox <snugfox@users.noreply.github.com>
2020-03-24 20:27:40 -0400

    Fix Docker login scope error in CI

--- build/release.sh
+++ build/release.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env sh
 
-BUILD_IMAGE="$1"
-
 echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 echo "${GITHUB_TOKEN}" | docker login docker.pkg.github.com -u snugfox --password-stdin
 
-docker run --rm -e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD -e GITHUB_TOKEN \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$(pwd):/go/src/github.com/snugfox/mcl" ${BUILD_IMAGE} \
-    goreleaser release --rm-dist
+goreleaser release --rm-dist
