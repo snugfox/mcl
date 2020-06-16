21dfd8edc7c823c8e77c90b56f9b04510e103eb3
snugfox <snugfox@users.noreply.github.com>
2020-06-16 19:48:21 -0400

    Add .editorconfig rules for non-Go files

--- build/docker-make.ps1
+++ build/docker-make.ps1
@@ -6,8 +6,8 @@ Write-Host "Built image $imageID"
 
 Write-Host "Running make in build image..."
 docker run --rm -it `
-    -e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD `
-    -e GITHUB_TOKEN `
-    -v //var/run/docker.sock:/var/run/docker.sock `
-    -v "${PWD}:/go/src/github.com/snugfox/mcl" `
-    $imageID make $args
+	-e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD `
+	-e GITHUB_TOKEN `
+	-v //var/run/docker.sock:/var/run/docker.sock `
+	-v "${PWD}:/go/src/github.com/snugfox/mcl" `
+	$imageID make $args
