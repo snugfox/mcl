66878450e14d4f2aa627a44924b8d05d0971b087
snugfox <snugfox@users.noreply.github.com>
2020-06-16 23:21:05 -0400

    Remove -it flags from docker run build commands

--- build/docker-make.ps1
+++ build/docker-make.ps1
@@ -5,7 +5,7 @@ $imageID = $(docker build -q -f ./build/Dockerfile.build .)
 Write-Host "Built image $imageID"
 
 Write-Host "Running make in build image..."
-docker run --rm -it `
+docker run --rm `
 	-e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD `
 	-e GITHUB_TOKEN `
 	-v //var/run/docker.sock:/var/run/docker.sock `
--- build/docker-make.sh
+++ build/docker-make.sh
@@ -7,7 +7,7 @@ image_id=$(docker build -q -f ./build/Dockerfile.build .)
 echo "Built image $image_id" >&2
 
 echo "Running make in build image..." >&2
-exec docker run --rm -it \
+exec docker run --rm \
   -e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD \
   -e GITHUB_TOKEN \
   -v /var/run/docker.sock:/var/run/docker.sock \
