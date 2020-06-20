3157f6fffa76cfce485d450c3734c6a822b3f9d0
snugfox <snugfox@users.noreply.github.com>
2020-06-20 16:08:42 -0400

    Simplify command handling in builder scripts

--- build/builder.ps1
+++ build/builder.ps1
@@ -25,34 +25,34 @@ $Tag        = $(Get-StringHash `
 							ToLowerInvariant()
 $Image      = "${Name}:${Tag}"
 
+$InitScriptBlock = {
+	docker image inspect -f "{{ .Id }}" "$Image" *>&1 | Out-Null
+	if (! $?) {
+		docker pull -q "$Image" *>&1 | Out-Null
+		if ($?) {
+			Write-Information "Pulled ${Image}"
+		} else {
+			docker build -q -f "$Dockerfile" -t "$Image" . | Out-Null
+			Write-Information "Built ${Image}"
+		}
+	}
+}
+
 $Command = ($args.Length -gt 0) ? $args[0] : ""
 switch ($Command) {
 	"image" {
 		Write-Host $Image
-		exit 0
 	}
-}
-
-docker image inspect -f "{{ .Id }}" "$Image" *>&1 | Out-Null
-if (! $?) {
-	docker pull -q "$Image" *>&1 | Out-Null
-	if ($?) {
-		Write-Information "Pulled ${Image}"
-	} else {
-		docker build -q -f "$Dockerfile" -t "$Image" . | Out-Null
-		Write-Information "Built ${Image}"
-	}
-}
-
-switch ($Command) {
 	"push" {
 		Write-Warning "It is advised not to push builder images built on Windows. To force a push, use the push-force command."
 	}
 	"push-force" {
+		& $InitScriptBlock
 		docker push "${Image}" | Out-Null
 		Write-Information "Pushed ${Image}"
 	}
 	"run" {
+		& $InitScriptBlock
 		docker run --rm `
 			-e DOCKERHUB_USERNAME -e DOCKERHUB_PASSWORD `
 			-e GITHUB_TOKEN `
