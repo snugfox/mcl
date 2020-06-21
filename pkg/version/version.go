9cc69fe5a28216a006a67eb5f04e664b4c64923e
snugfox <snugfox@users.noreply.github.com>
2020-06-21 19:58:40 -0400

    version: init version info if empty at runtime

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,9 +1,25 @@
 package version
 
+import (
+	"runtime"
+)
+
 // Build information that is populated at compile-time
 var (
 	BuildDate string
-	GoVersion string
+	GoVersion string = runtime.Version()
 	Revision  string
 	Version   string
 )
+
+func init() {
+	if BuildDate == "" {
+		BuildDate = "0000-00-00T00:00:00Z"
+	}
+	if Revision == "" {
+		Revision = "0000000"
+	}
+	if Version == "" {
+		Version = "v0.0.0-unknown"
+	}
+}
