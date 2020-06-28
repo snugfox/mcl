b28cc6ff5367a748cbe2e19fb4aeae73024b323b
snugfox <snugfox@users.noreply.github.com>
2020-06-28 17:10:28 -0400

    cmd: rework list-versions command

--- cmd/mcl/app/listversions.go
+++ cmd/mcl/app/listversions.go
@@ -3,68 +3,60 @@ package app
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-
-	"github.com/snugfox/mcl/internal/bundle"
 )
 
 // ListVersionsFlags contains the flags for the MCL list-versions command
-type ListVersionsFlags struct {
-	Edition string
-	Offline bool
-}
+type ListVersionsFlags struct{}
 
 // NewListVersionsFlags returns a new ListVersionsFlags object with default
 // parameters
 func NewListVersionsFlags() *ListVersionsFlags {
-	return &ListVersionsFlags{
-		Edition: "",    // Required flag
-		Offline: false, // Query versions available online
-	}
+	return &ListVersionsFlags{}
 }
 
 // FlagSet returns a new pflag.FlagSet with MCL list-versions command flags
 func (lvf *ListVersionsFlags) FlagSet() *pflag.FlagSet {
 	fs := pflag.NewFlagSet("list-versions", pflag.ExitOnError)
-	fs.StringVar(&lvf.Edition, "edition", lvf.Edition, "Minecraft edition")
-	fs.BoolVar(&lvf.Offline, "offline", lvf.Offline, "Only list versions currently available offline")
-	fs.MarkHidden("offline") // Not yet implemented
 	return fs
 }
 
 // NewListVersionsCommand creates a new *cobra.Command for the MCL list-versions
 // command with default flags.
 func NewListVersionsCommand() *cobra.Command {
-	listVersionsFlags := NewListVersionsFlags()
+	lvf := NewListVersionsFlags()
 
 	cmd := &cobra.Command{
 		Use:   "list-versions",
 		Short: "Lists available versions for a specified edition",
-		Run: func(cmd *cobra.Command, _ []string) {
-			ctx := context.Background()
-
-			// Resolve edition to its provider
-			edition := listVersionsFlags.Edition
-			p, ok := bundle.NewProviderBundle()[edition]
-			if !ok {
-				log.Fatalln("Provider not found")
-			}
-
-			// Print versions returned form the provider
-			versions, err := p.Versions(ctx)
-			if err != nil {
-				log.Fatalln("Failed to retrieve versions:", err)
-			}
-			for i := range versions {
-				fmt.Println(versions[i])
-			}
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runListVersions(cmd.Context(), lvf, args[0])
 		},
 	}
 
-	cmd.PersistentFlags().AddFlagSet(listVersionsFlags.FlagSet())
+	flags := cmd.Flags()
+	flags.SetInterspersed(false)
+
+	flags.AddFlagSet(lvf.FlagSet())
 
 	return cmd
+}
+
+func runListVersions(ctx context.Context, lvf *ListVersionsFlags, ed string) error {
+	p, ok := cmdBundle[ed]
+	if !ok {
+		return fmt.Errorf("no provider exists for edition %s", ed)
+	}
+	vers, err := p.Versions(ctx)
+	if err != nil {
+		return err
+	}
+	for i := range vers {
+		fmt.Println(vers[i])
+	}
+
+	return nil
 }
